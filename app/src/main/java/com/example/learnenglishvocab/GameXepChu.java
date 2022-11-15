f4316eb1a2b64baa602191f771aa28ff25f8bb86
= <=>
2022-11-15 10:58:21 +0700

    create game xep chu

---
+++ app/src/main/java/com/example/learnenglishvocab/GameXepChu.java
@@ -0,0 +1,14 @@
+package com.example.learnenglishvocab;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GameXepChu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game_xep_chu);
+    }
+}
