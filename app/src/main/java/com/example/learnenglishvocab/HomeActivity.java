58acadd11fd36abecebf9f7ed3417e2a585693b9
Thquan105 <thquan105@gmail.com>
2022-11-18 15:19:11 +0700

    Add event NavBar

--- app/src/main/java/com/example/learnenglishvocab/HomeActivity.java
+++ app/src/main/java/com/example/learnenglishvocab/HomeActivity.java
@@ -1,16 +1,136 @@
 package com.example.learnenglishvocab;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
 
 import com.google.android.material.navigation.NavigationView;
 
-public class HomeActivity extends AppCompatActivity{
+public class HomeActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
+
+    //Variables
+    static final float END_SCALE = 1f;
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+
+    ImageView btMenu;
+    RelativeLayout contentView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        //Hooks
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.navigation_view);
+        btMenu = findViewById(R.id.btn_menu);
+        contentView = findViewById(R.id.content);
+
+        navigationDrawer();
+
+
+
+
+    }
+
+    private void navigationDrawer(){
+        //Navigation Drawer
+        navigationView.bringToFront();
+        navigationView.setNavigationItemSelectedListener(this);
+
+        btMenu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(drawerLayout.isDrawerVisible(GravityCompat.START)) {
+                    drawerLayout.closeDrawer(GravityCompat.START);
+                }
+                else {
+                    drawerLayout.openDrawer(GravityCompat.START);
+                }
+            }
+        });
+
+        animateNaviagtionDrawer();
+    }
+
+    private void animateNaviagtionDrawer() {
+        //drawerLayout.setScrimColor(getResources().getColor(R.color.colorPrimary));
+        drawerLayout.addDrawerListener(new DrawerLayout.SimpleDrawerListener() {
+            @Override
+            public void onDrawerSlide(View drawerView, float slideOffset) {
+                // Scale the View based on current slide offset
+                final float diffScaledOffset = slideOffset * (1 - END_SCALE);
+                final float offsetScale = 1 - diffScaledOffset;
+                contentView.setScaleX(offsetScale);
+                contentView.setScaleY(offsetScale);
+                // Translate the View, accounting for the scaled width
+                final float xOffset = drawerView.getWidth() * slideOffset;
+                final float xOffsetDiff = contentView.getWidth() * diffScaledOffset / 2;
+                final float xTranslation = xOffset - xOffsetDiff;
+                contentView.setTranslationX(xTranslation);
+            }
+        });
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }
+        else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId())
+        {
+            case R.id.profile:
+            {
+                Toast.makeText(HomeActivity.this,"login",Toast.LENGTH_SHORT).show();
+                break;
+            }
+            case R.id.notification:
+            {
+
+            }
+            case R.id.hengio:
+            {
+
+            }
+            case R.id.share:
+            {
+
+            }
+            case R.id.rate:
+            {
+
+            }
+            case R.id.feedback:
+            {
+
+            }
+            case R.id.about:
+            {
+
+            }
+            case R.id.logout:
+            {
+
+            }
+        }
+        return false;
     }
 }
