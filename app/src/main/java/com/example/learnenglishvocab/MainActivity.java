2f9faacdfbf4ff457a793329b716b12bda1ca926
TrVanChinh <chinhtran0964758845@gmail.com>
2022-11-19 22:36:40 +0700

    firebase

--- app/src/main/java/com/example/learnenglishvocab/MainActivity.java
+++ app/src/main/java/com/example/learnenglishvocab/MainActivity.java
@@ -9,14 +9,23 @@ import android.widget.Button;
 import android.widget.GridLayout;
 
 import com.example.learnenglishvocab.Adapter.Game1Adapter;
+import com.google.firebase.auth.FirebaseAuthException;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
+    FirebaseFirestore firestore;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game1);
+
+        firestore = FirebaseFirestore.getInstance();
+        Map<String,Object> user = new HashMap<>();
+        user.put("firstName","Easy");
     }
 }
