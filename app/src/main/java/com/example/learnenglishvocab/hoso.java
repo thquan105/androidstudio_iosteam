a6abd1420f10f2305af629310ca879087e6f0986
tluyen2602@gmail.com <tluyen2602@gmail.com>
2022-11-21 21:32:43 +0700

    update caidat

--- app/src/main/java/com/example/learnenglishvocab/hoso.java
+++ app/src/main/java/com/example/learnenglishvocab/hoso.java
@@ -1,9 +1,12 @@
 package com.example.learnenglishvocab;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Switch;
 
 public class hoso extends AppCompatActivity {
 
@@ -17,5 +20,18 @@ public class hoso extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_chinhsuahoso,menu);
         return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.itemchinhsuataikhoan:
+                break;
+            case R.id.itemchinhsuattcn:
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
+
     }
 }
