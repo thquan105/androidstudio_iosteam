054edebd2c7426b4bef829b23361676ad59a95cf
= <=>
2022-11-27 09:46:38 +0700

    check dung sai, center chu trong gridlayout

--- app/src/main/java/com/example/learnenglishvocab/GameXepChu.java
+++ app/src/main/java/com/example/learnenglishvocab/GameXepChu.java
@@ -1,18 +1,19 @@
 package com.example.learnenglishvocab;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.Dialog;
 import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Random;
 
 public class GameXepChu extends AppCompatActivity implements InterfaceClickChu {
@@ -21,13 +22,23 @@ public class GameXepChu extends AppCompatActivity implements InterfaceClickChu {
     private ArrayList<String> arrChu, arrAnswer,arrDung;
     private TuKhoaAdapter chuAdapter,dapAnAdapter;
     private ImageButton imgbtnRefresh;
-    private String tuTA = "pencil";
+    private Dialog dialogDung,dialogSai;
+    private String tuTA = "recycler";
+    final private int maxCotGridlayout = 7;
+    private GridLayoutManager gridLayoutManagerchu,gridLayoutManagerAnswer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_xep_chu);
         init();
+        CardView cvBack = findViewById(R.id.cvback);
+        cvBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
 
     private void init(){
@@ -55,15 +66,16 @@ public class GameXepChu extends AppCompatActivity implements InterfaceClickChu {
 
         arrChu = new ArrayList<>();
         arrDung = new ArrayList<>();
+        arrAnswer = new ArrayList<>();
 
         for(String i: strSplit){
             if (i.equals("")==false)
                 arrDung.add(i);
         }
 
-        arrChu.addAll(shuffleArray(arrDung));
+        arrChu.addAll(arrDung);
+        shuffleArray(arrChu);
 
-        arrAnswer = new ArrayList<>();
 
         //Làm mới rcv
         imgbtnRefresh.setOnClickListener(new View.OnClickListener() {
@@ -76,28 +88,47 @@ public class GameXepChu extends AppCompatActivity implements InterfaceClickChu {
         chuAdapter = new TuKhoaAdapter(arrChu,this);
         dapAnAdapter = new TuKhoaAdapter(arrAnswer,this);
 
-        GridLayoutManager gridLayoutManagerchu,gridLayoutManagerAnswer;
-        gridLayoutManagerchu = new GridLayoutManager(this,7);
-        gridLayoutManagerAnswer = new GridLayoutManager(this,7);
+        int cotChuMax = arrChu.size();
+        if (cotChuMax>7)
+            cotChuMax = 7;
+        gridLayoutManagerchu = new GridLayoutManager(this,cotChuMax);
         rcvChu.setLayoutManager(gridLayoutManagerchu);
-        rcvAnswer.setLayoutManager(gridLayoutManagerAnswer);
 
         chuAdapter = new TuKhoaAdapter(arrChu, new InterfaceClickChu() {
             @Override
             public void onItemClickChu(String chu) {
                 arrAnswer.add(chu);
+                int cotAnsmax = arrAnswer.size();
+                if (cotAnsmax>6)
+                    cotAnsmax = 6;
+                gridLayoutManagerAnswer = new GridLayoutManager(GameXepChu.this,cotAnsmax);
+                rcvAnswer.setLayoutManager(gridLayoutManagerAnswer);
                 dapAnAdapter.notifyDataSetChanged();
                 if(arrAnswer.size()==arrChu.size()){
+
                     int check = 0;
-                    for (int i = 0; i < arrChu.size();i++){
-                        if(arrChu.get(i).equals(arrDung.get(i))==true){
+                    for (int i = 0; i < arrAnswer.size();i++){
+                        if(arrAnswer.get(i).equals(arrDung.get(i))==true){
                             check++;
                         }
                     }
-                    if(check==arrChu.size()){
-                        Toast.makeText(GameXepChu.this,"Thông minh lắm",Toast.LENGTH_SHORT).show();
+                    if(check==arrDung.size()){
+                        dialogDung = new Dialog(GameXepChu.this);
+                        dialogDung.setContentView(R.layout.dialog_game2);
+
+                        Button btnnext =dialogDung.findViewById(R.id.btnDialoggame2);
+                        btnnext.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view) {
+                                dialogDung.dismiss();
+                                Lammoircv();
+                            }
+                        });
+                        dialogDung.show();
                     }
                     else{
+                        for (String s: arrDung)
+                            System.out.println(s);
                         Toast.makeText(GameXepChu.this,"Đồ ngu",Toast.LENGTH_SHORT).show();
                         arrChu.removeAll(arrChu);
                         arrChu.addAll(shuffleArray(arrDung));
@@ -105,7 +136,6 @@ public class GameXepChu extends AppCompatActivity implements InterfaceClickChu {
                     }
                 }
 
-//                goDetail(phim);
 //                Toast.makeText(GameXepChu.this,chu,Toast.LENGTH_SHORT).show();
 //                for (int i = 0; i < arrChu.size();i++){
 //                    if(arrChu.get(i).equals(chu)==true){
@@ -125,6 +155,7 @@ public class GameXepChu extends AppCompatActivity implements InterfaceClickChu {
     private void Lammoircv() {
         arrAnswer.removeAll(arrAnswer);
         dapAnAdapter.notifyDataSetChanged();
+        shuffleArray(arrChu);
         rcvChu.setAdapter(chuAdapter);
         rcvAnswer.setAdapter(dapAnAdapter);
     }
@@ -139,16 +170,6 @@ public class GameXepChu extends AppCompatActivity implements InterfaceClickChu {
             String a = ar.get(index);
             ar.set(index,ar.get(i));
             ar.set(i,a);
-        }
-        return ar;
-    }
-    private String[] shuf2fleArray(String[] ar) {
-        Random rnd = new Random();
-        for (int i = ar.length - 1; i > 0; i--) {
-            int index = rnd.nextInt(i + 1);
-            String a = ar[index];
-            ar[index] = ar[i];
-            ar[i] = a;
         }
         return ar;
     }
