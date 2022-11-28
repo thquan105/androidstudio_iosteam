5c3f528195e8a277c8190ff99088f30ef7314d1c
TrVanChinh <chinhtran0964758845@gmail.com>
2022-11-28 10:24:12 +0700

    translation

--- app/src/main/java/com/example/learnenglishvocab/TranslationActivity.java
+++ app/src/main/java/com/example/learnenglishvocab/TranslationActivity.java
@@ -2,13 +2,97 @@ package com.example.learnenglishvocab;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.SurfaceControl;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.PopupMenu;
+import android.widget.TextView;
+
+import com.google.android.material.button.MaterialButton;
+import com.google.mlkit.nl.translate.TranslateLanguage;
+import com.google.mlkit.nl.translate.Translator;
+import com.google.mlkit.nl.translate.TranslatorOptions;
+
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
 
 public class TranslationActivity extends AppCompatActivity {
+
+    private EditText sourceLanguageEt;
+    private TextView destinationLanguageTv;
+    private MaterialButton sourceLanguageChooseBtn, destinationLanguageChooseBtn, translateBtn;
+    private TranslatorOptions translatorOptions;
+    private Translator translator;
+    private ProgressDialog processDialog;
+    private ArrayList<ModelLanguage> languageArrayList;
+
+    private static final String TAG="MAIN_TAG";
+
+    public TranslationActivity() {
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_translation);
+
+        sourceLanguageEt = findViewById(R.id.sourceLanguageEt);
+        destinationLanguageTv = findViewById(R.id.destinationLanguageTv);
+        sourceLanguageChooseBtn = findViewById(R.id.sourceLanguageChooseBtn);
+        destinationLanguageChooseBtn = findViewById(R.id.destinationLanguageChooseBtn);
+        translateBtn = findViewById(R.id.translateBtn);
+
+        processDialog = new ProgressDialog(this);
+        processDialog.setTitle("Please wait");
+        processDialog.setCanceledOnTouchOutside(false);
+
+        loadAvailableLanguages();
+
+        sourceLanguageChooseBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        destinationLanguageChooseBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        translateBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+    }
+
+    private void SourceLanguageChoose(){
+        PopupMenu popupMenu = new PopupMenu(this,sourceLanguageChooseBtn);
+
+    }
+
+    private void loadAvailableLanguages() {
+        languageArrayList = new ArrayList<>();
+        List<String> languageCodeList = TranslateLanguage.getAllLanguages();
+
+        for (String languageCode: languageCodeList){
+            String languageTitle = new Locale(languageCode).getDisplayLanguage();
+            Log.d(TAG,"loadAvailableLanguages: languageCode: "+languageCode);
+            Log.d(TAG,"loadAvailableLanguages: languageTitle: "+languageTitle);
+
+            ModelLanguage modelLanguage = new ModelLanguage(languageCode,languageTitle);
+            languageArrayList.add(modelLanguage);
+        }
+
     }
 }
