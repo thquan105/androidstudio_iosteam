3b1d1a46cc9a027a030c04f04bcbb58f3efaca85
Thquan105 <thquan105@gmail.com>
2022-12-02 19:32:57 +0700

    add event click card nhom tu

--- app/src/main/java/com/example/learnenglishvocab/Adapter/CategoryAdapter.java
+++ app/src/main/java/com/example/learnenglishvocab/Adapter/CategoryAdapter.java
@@ -1,6 +1,7 @@
 package com.example.learnenglishvocab.Adapter;
 
 import android.content.Context;
+import android.content.Intent;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,10 +12,13 @@ import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.request.RequestOptions;
+import com.example.learnenglishvocab.GameChonHinh;
+import com.example.learnenglishvocab.GameXepChu;
 import com.example.learnenglishvocab.Models.Category;
 import com.example.learnenglishvocab.R;
 import com.google.firebase.firestore.AggregateQuery;
@@ -36,9 +40,9 @@ import de.hdodenhof.circleimageview.CircleImageView;
 public class CategoryAdapter extends RecyclerView.Adapter<CategoryAdapter.myViewHoder> {
     Context context;
     ArrayList<Category> dsCategory;
-    boolean a;
+    int a;
 
-    public CategoryAdapter(Context context, ArrayList<Category> dsCategory, boolean a) {
+    public CategoryAdapter(Context context, ArrayList<Category> dsCategory, int a) {
         this.context = context;
         this.dsCategory = dsCategory;
         this.a = a;
@@ -48,7 +52,7 @@ public class CategoryAdapter extends RecyclerView.Adapter<CategoryAdapter.myView
     @Override
     public myViewHoder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
 
-        if (a) {
+        if (a == 1) {
             View view = LayoutInflater.from(context).inflate(R.layout.item_category, parent, false);
             return new myViewHoder(view);
         } else {
@@ -62,7 +66,7 @@ public class CategoryAdapter extends RecyclerView.Adapter<CategoryAdapter.myView
         Category category = dsCategory.get(position);
 
         holder.name.setText(category.getTenNhomTu());
-
+        //Set hinhanh
         Glide
                 .with(holder.img.getContext())
                 .load(category.getHinhanh())
@@ -71,7 +75,7 @@ public class CategoryAdapter extends RecyclerView.Adapter<CategoryAdapter.myView
                     .fitCenter()
                     .error(com.google.android.gms.base.R.drawable.common_google_signin_btn_icon_dark_normal))
                 .into(holder.img);
-
+        //Dem so tu cua nhom tu
         FirebaseFirestore mydb;
         mydb = FirebaseFirestore.getInstance();
         AggregateQuery countQuery = mydb.collection("TuVung")
@@ -85,6 +89,28 @@ public class CategoryAdapter extends RecyclerView.Adapter<CategoryAdapter.myView
                 System.out.println( "Count failed: "+ task.getException());
             }
         });
+        //Click Nhom tu
+        if (a == 1) {
+            holder.cv_game_1.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    //Truyen IDNhomTu sang game 1
+                    Intent intent = new Intent(view.getContext(), GameChonHinh.class);
+                    intent.putExtra("IDNhom", category.getIDNhomTu());
+                    view.getContext().startActivity(intent);
+                }
+            });
+        } else {
+            holder.cv_game_2.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    //Truyen IDNhomTu sang game 2
+                    Intent intent = new Intent(view.getContext(), GameXepChu.class);
+                    intent.putExtra("IDNhom", category.getIDNhomTu());
+                    view.getContext().startActivity(intent);
+                }
+            });
+        }
     }
 
 
@@ -96,6 +122,7 @@ public class CategoryAdapter extends RecyclerView.Adapter<CategoryAdapter.myView
     class myViewHoder extends RecyclerView.ViewHolder{
         ImageView img;
         TextView name, numWord;
+        CardView cv_game_1, cv_game_2;
 
         public myViewHoder(@NonNull View itemView) {
             super(itemView);
@@ -103,6 +130,8 @@ public class CategoryAdapter extends RecyclerView.Adapter<CategoryAdapter.myView
             img = (ImageView) itemView.findViewById(R.id.img_avatar_cate);
             name = (TextView) itemView.findViewById(R.id.tv_Categary);
             numWord = (TextView) itemView.findViewById(R.id.tv_sotu);
+            cv_game_1 = (CardView) itemView.findViewById(R.id.game_card_1);
+            cv_game_2 = (CardView) itemView.findViewById(R.id.game_card_2);
         }
     }
 }
